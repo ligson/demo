446c55419c0171a76801cb503241227f097f348a
ligson519241@gmail.com <ligson519241@gmail.com@3384305c-e22e-deec-bfe7-e2019ea7cb57>
2013-03-13 09:43:29 +0000

    using h2 database
    git-svn-id: https://ligson521.googlecode.com/svn/trunk/demo@12 3384305c-e22e-deec-bfe7-e2019ea7cb57

--- src/main/java/demo/util/SystemInit.java
+++ src/main/java/demo/util/SystemInit.java
@@ -1,11 +1,22 @@
 package demo.util;
 
+import java.io.File;
+
 import org.springframework.beans.factory.InitializingBean;
 
 public class SystemInit implements InitializingBean {
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
+		String userdir = System.getProperty("user.dir");
+		File file = new File(userdir,".demo");
+		if(!file.exists()){
+			file.mkdirs();
+		}
+		File dbFile = new File(file,"demodb");
+		if(!dbFile.exists()){
+			dbFile.createNewFile();
+		}
 		System.out.println("http://127.0.0.1:8080/demo");
 	}
 
